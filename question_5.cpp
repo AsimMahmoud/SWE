6749ee23bf6866214458c1104f3359fddcd44883
AsimMahmoud <3asim.ma7moud@gmail.com>
2015-10-22 06:36:49 +0200

    Update question_5.cpp

--- question_5.cpp
+++ question_5.cpp
@@ -1,1 +1,42 @@
+#include <iostream>
+#include <string> 
+using namespace std;
+class bank_account
+{
 
+public:
+	bank_account();
+	bank_account(const string dep, string ac_n, int bln);
+	void display();
+	void deposit(int inc);
+	void withdraw(int dec);
+private:
+	std::string   depositor;
+	std::string	  account_no;
+	int balance;
+};
+
+bank_account::bank_account()
+{
+	depositor  = "no name";
+	account_no = "0000" ;
+	balance    = 0 ;
+}
+bank_account::bank_account(string dep, string ac_n, int bln)
+{
+	depositor  = dep ;
+	account_no = ac_n ;
+	balance    = bln ;
+}
+void bank_account::display()
+{
+	cout << depositor << "\n" << account_no << "\n" << balance;
+}
+void bank_account::deposit(int inc)
+{
+	balance += inc;
+}
+void bank_account::withdraw(int dec)
+{
+	balance -= dec;
+}
